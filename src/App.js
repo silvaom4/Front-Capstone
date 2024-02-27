c329b2c1ac9ce1a2f8458a7047ecf5b60186a86c
LampkinSynK <cejajaylin5@gmail.com>
2024-02-27 14:22:26 -0500

    added routing

--- src/App.js
+++ src/App.js
@@ -4,11 +4,21 @@ import "./assets/vendor/nucleo/css/nucleo.css";
 import "./assets/vendor/font-awesome/css/font-awesome.min.css";
 import "./assets/scss/argon-design-system-react.scss?v1.1.0";
 import CotntSum from './components/CotntSum';
+import { BrowserRouter as Router, Route, Routes, Redirect } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <CotntSum />
+      <Router>
+        <div>
+          <Routes>
+            <Route path="/" element={<CotntSum />}></Route>
+
+          </Routes>
+          
+          
+        </div>
+      </Router>
     </div>
   );
 }
