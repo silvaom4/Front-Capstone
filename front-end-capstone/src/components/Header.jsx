e53461c4bb064876be69f944903b566e2bfa4b45
Sincerekey <sincerelykeyondre@gmail.com>
2024-02-27 20:32:17 -0500

    update

--- front-end-capstone/src/components/Header.jsx
+++ front-end-capstone/src/components/Header.jsx
@@ -1,16 +1,136 @@
 import React from "react";
-import {Navbar, Container} from "reactstrap";
+// import {Navbar, Container, NavbarBrand, NavbarText, Row, Col} from "reactstrap";
+import {
+    UncontrolledCollapse,
+    DropdownMenu,
+    DropdownItem,
+    UncontrolledDropdown,
+    NavbarBrand,
+    Navbar,
+    NavItem,
+    NavLink,
+    NavbarText,
+    Nav,
+    Container,
+    Row,
+    Col
+  } from "reactstrap";
 
 function Header() {
   return (
     <div>
-       <Navbar
+       {/* <Navbar
           className="navbar-horizontal navbar-dark bg-primary"
           expand="lg"
         >
         <Container>
-<h2>Header</h2>
+    <NavbarBrand  href="#" onClick={e => e.preventDefault()}>
+              Header Text
+            </NavbarBrand>
+            <Row>
+                <Col>
+            <NavbarText active>
+              Header Text
+              </NavbarText>
+              </Col>
+              <Col>
+            <NavbarText active>
+              Header Text
+              </NavbarText>
+              </Col>
+              <Col>
+            <NavbarText active>
+              Header Text
+              </NavbarText>
+              </Col>
+              </Row>
         </Container>
+        </Navbar> */}
+        <Navbar
+          className="navbar-horizontal navbar-dark bg-primary mt-4"
+          expand="lg"
+        >
+          <Container>
+            {/* <NavbarBrand href="#pablo" onClick={e => e.preventDefault()}> */}
+              <h2>iSummarize</h2>
+            {/* </NavbarBrand> */}
+            <button
+              className="navbar-toggler"
+              data-target="#navbar-primary"
+              data-toggle="collapse"
+              id="navbar-primary"
+              type="button"
+            >
+              <span className="navbar-toggler-icon" />
+            </button>
+            <UncontrolledCollapse navbar toggler="#navbar-primary">
+              <div className="navbar-collapse-header">
+                <Row>
+                  <Col className="collapse-brand" xs="6">
+                    <hi>Close</hi>
+                  </Col>
+                  <Col className="collapse-close" xs="6">
+                    <button
+                      className="navbar-toggler"
+                      data-target="#navbar-primary"
+                      data-toggle="collapse"
+                      id="navbar-primary"
+                      type="button"
+                    >
+                      <span />
+                      <span />
+                    </button>
+                  </Col>
+                </Row>
+              </div>
+              <Nav className="ml-lg-auto" navbar>
+                <NavItem>
+                  <NavLink href="#pablo" onClick={e => e.preventDefault()}>
+                    Fourm 
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="#pablo" onClick={e => e.preventDefault()}>
+                    About Us
+                  </NavLink>
+                </NavItem>
+                <UncontrolledDropdown nav>
+                  <NavLink
+                    data-toggle="dropdown"
+                    href="#"
+                    id="navbar-primary_dropdown_1"
+                    onClick={e => e.preventDefault()}
+                    role="button"
+                  >
+                    Contact Us
+                  </NavLink>
+                  <DropdownMenu
+                    right
+                  >
+                    <DropdownItem
+                      href="#"
+                      onClick={e => e.preventDefault()}
+                    >
+                      Action
+                    </DropdownItem>
+                    <DropdownItem
+                      href="#"
+                      onClick={e => e.preventDefault()}
+                    >
+                      Another action
+                    </DropdownItem>
+                    <DropdownItem divider />
+                    <DropdownItem
+                      href="#"
+                      onClick={e => e.preventDefault()}
+                    >
+                      Something else here
+                    </DropdownItem>
+                  </DropdownMenu>
+                </UncontrolledDropdown>
+              </Nav>
+            </UncontrolledCollapse>
+          </Container>
         </Navbar>
     </div>
   )
